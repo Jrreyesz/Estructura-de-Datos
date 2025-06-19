9f0550eb004670c9f22b4da5281db7a23748c2c0
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-19 16:24:39 -0500

    Avance

---
+++ Práctico_Experimental_1/Cita.cs
@@ -0,0 +1,21 @@
+public struct Cita
+{
+    private string Nombre;
+    private string Apellidos;
+    private DateTime FechaHora;
+
+    public string nombre { get => Nombre; set => Nombre = value; }
+    public string apellidos { get => Apellidos; set => Apellidos = value; }
+    public DateTime fechahora { get => FechaHora; set => FechaHora = value; }
+
+    public void crearCita(string _Nombre, string _Apellidos, DateTime _FechaHora)
+    {
+        this.Nombre = _Nombre;
+        this.Apellidos = _Apellidos;
+        this.FechaHora = _FechaHora;
+    }
+
+
+
+
+}
