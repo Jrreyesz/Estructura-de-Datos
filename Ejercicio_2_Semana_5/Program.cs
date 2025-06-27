bd51e8b16a9b697a7e67f224949d1dc305048af8
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 15:41:34 -0500

    Fin Ejercicio 2

--- Ejercicio_2_Semana_5/Program.cs
+++ Ejercicio_2_Semana_5/Program.cs
@@ -5,14 +5,17 @@
         List<string> materias = ["Matemática", "Física", "Química", "Historia", "Lengua"];
         List<string> notas = [];
 
-        for (int i = 0; i < materias.Count; i++) ;
-        System.Console.WriteLine($"Ingrese su nota en la materia de {materias[i]}: ");
-        string nota = Consolo.ReadLine();
-        notas.Add[nota];
-
-        for (int i = 0; i < materias.Count; i++) ;
+        for (int i = 0; i < materias.Count; i++)
         {
-            System.Console.WriteLine($"En asignatura {materias[i]} has sacado una nota de {notas[i]}");
+            System.Console.WriteLine($"Ingrese su nota en la materia de {materias[i]}: ");
+            string nota = Console.ReadLine();
+            notas.Add(nota);
         }
+
+        for (int i = 0; i < materias.Count; i++)
+        {
+            System.Console.WriteLine($"En {materias[i]} has sacado {notas[i]}");
+        }
+
     }
 }
