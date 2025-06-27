2818f46068507a8e2818d67f6d3254570fe2741a
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 15:11:44 -0500

    Fin ejercicio 1

--- Ejercicio_1_Semana_5/Program.cs
+++ Ejercicio_1_Semana_5/Program.cs
@@ -4,8 +4,6 @@
     {
         List<string> asignaturas = ["Matemática", "Física", "Química", "Historia", "Lengua"];
 
-        System.Console.WriteLine($"Lista de materias: {asignaturas}");
-
         foreach (var asignatura in asignaturas)
         {
             System.Console.WriteLine($"Materia: {asignatura}");
