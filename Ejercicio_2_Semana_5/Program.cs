a614b16379afaa3cb989abc640a0224481e8396e
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 15:27:30 -0500

    Continuación E2

--- Ejercicio_2_Semana_5/Program.cs
+++ Ejercicio_2_Semana_5/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> materias = ["Matemática", "Física", "Química", "Historia", "Lengua"];
+        List<string> notas = [];
+
+        for (int i = 0; i < materias.Count; i++) ;
+        System.Console.WriteLine($"Ingrese su nota en la materia de {materias[i]}: ");
+        string nota = Consolo.ReadLine();
+        notas.Add[nota];
+
+        for (int i = 0; i < materias.Count; i++) ;
+        {
+            System.Console.WriteLine($"En asignatura {materias[i]} has sacado una nota de {notas[i]}");
+        }
+    }
+}
