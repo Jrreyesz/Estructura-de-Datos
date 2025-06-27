0e18417bcff8c9bf57512023d9b3382d7267fba4
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 17:34:42 -0500

    Final E5

--- Ejercicio_5_Semana_5/Program.cs
+++ Ejercicio_5_Semana_5/Program.cs
@@ -6,32 +6,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<string> asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"];
-        List<double> notas = [];
+        List<string> asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]; //Lista de asignaturas
+        List<double> notas = []; //Lista de notas
 
+        //Bucle para ingresar las notas de cada materia
         for (int i = 0; i < asignaturas.Count; i++)
         {
             Console.WriteLine($"Ingrese la nota de la materia de {asignaturas[i]}: ");
             string entrada = Console.ReadLine();
             double nota;
 
-            // Validar que la entrada sea un número
+            // Validar que la entrada sea un número para convertirlo a float
             while (!double.TryParse(entrada, NumberStyles.Any, CultureInfo.InvariantCulture, out nota))
             {
                 Console.WriteLine("Entrada inválida. Ingrese un número válido:");
                 entrada = Console.ReadLine();
             }
 
+            //Añadimos la nota ya en float a la lista de notas
             notas.Add(nota);
         }
 
-        for (int i = 0; i < notas.Count; i++)
-        {
-            Console.WriteLine($"{asignaturas[i]}: {notas[i]}");
-        }
-
+        //Creamos una lista de materias reprobadas
         List<string> reprobadas = [];
 
+        //Bucle para evaluar que materias se reprobaron para añadirlas a la lista de reprobadas
         for (int i = 0; i < asignaturas.Count; i++)
         {
             if (notas[i] < 7)
@@ -44,6 +43,7 @@ class Program
         System.Console.WriteLine("Materias Reprobadas");
         System.Console.WriteLine("");
 
+        //Bucle que imprime las materias reprobadas
         foreach (var materia in reprobadas)
         {
             System.Console.WriteLine(materia);
