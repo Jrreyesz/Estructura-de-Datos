e65456c0c45fbf92cff2f221468d92d18d25a1f0
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 15:51:59 -0500

    Cotinuación E3

--- Ejercicio_3_Semana_5/Program.cs
+++ Ejercicio_3_Semana_5/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        List<string> numsGanadores = [];
+
+        for (int i = 0; i < 6; i++)
+        {
+            System.Console.WriteLine($"Ingrese el número {i} ganador de la loteria: ");
+            string numGanador = Console.ReadLine;
+            numsGanadores.Add(numGanador);
+        }
+
+        numsGanadores.Sort();
+
+        foreach (var numero in numsGanadores)
+        {
+            System.Console.WriteLine(numero);
+        }
+    }
+}
