42a0e446917a7147fc7fc5a62d68dbd5f7be9781
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 16:14:25 -0500

    Continuación E4

--- Ejercicio_4_Semana_5/Program.cs
+++ Ejercicio_4_Semana_5/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        numeros.Reverse();
+
+        string listaDeNumeros = string.Join(", ", numeros);
+        System.Console.WriteLine(listaDeNumeros);
+
+    }
+}
