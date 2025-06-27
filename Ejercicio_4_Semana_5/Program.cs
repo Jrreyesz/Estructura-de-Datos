993cc9e3ef0fa8029aeb7daf4b6ee6033e12c316
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 17:36:01 -0500

    Código comentado

--- Ejercicio_4_Semana_5/Program.cs
+++ Ejercicio_4_Semana_5/Program.cs
@@ -1,11 +1,14 @@
 ﻿class Program
 {
     static void Main(string[] args)
-    {
+    {   
+        //Creamos una lista de numero del 1 al 10
         List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+        //Utilizamos el método reverse para revertir la lista
         numeros.Reverse();
         
+        //Imprimimos los números separados por coma en una sola linea
         string listaDeNumeros = string.Join(", ", numeros);
         System.Console.WriteLine(listaDeNumeros);
 
