8971056f67b19e50a2539cf90b61531a167f0142
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 15:56:03 -0500

    Continuación E3

--- Ejercicio_3_Semana_5/Program.cs
+++ Ejercicio_3_Semana_5/Program.cs
@@ -8,10 +8,16 @@
         for (int i = 0; i < 6; i++)
         {
             System.Console.WriteLine($"Ingrese el número {i} ganador de la loteria: ");
-            string numGanador = Console.ReadLine;
+            string numGanador = Console.ReadLine();
             numsGanadores.Add(numGanador);
         }
 
+        System.Console.WriteLine("");
+        System.Console.WriteLine("============================================================");
+        System.Console.WriteLine("=============Números ordenados de menor a mayor=============");
+        System.Console.WriteLine("============================================================");
+        System.Console.WriteLine("");
+        
         numsGanadores.Sort();
 
         foreach (var numero in numsGanadores)
