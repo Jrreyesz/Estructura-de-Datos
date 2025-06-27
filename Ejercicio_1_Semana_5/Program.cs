b1990acf689873f1907faf7e389cef8a628c0c05
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 15:09:40 -0500

    Continuación E1

--- Ejercicio_1_Semana_5/Program.cs
+++ Ejercicio_1_Semana_5/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> asignaturas = ["Matemática", "Física", "Química", "Historia", "Lengua"];
+
+        System.Console.WriteLine($"Lista de materias: {asignaturas}");
+
+        foreach (var asignatura in asignaturas)
+        {
+            System.Console.WriteLine($"Materia: {asignatura}");
+        }
+    }
+}
