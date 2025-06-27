63d1dbbda4559c1f20c868a6c02d73610a414305
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 17:38:24 -0500

    Código comentado

--- Ejercicio_3_Semana_5/Program.cs
+++ Ejercicio_3_Semana_5/Program.cs
@@ -2,13 +2,16 @@
 {
     static void Main(string[] args)
     {
-
+        //Creamos una lista para los numeros ganadores
         List<string> numsGanadores = [];
-
+        
+        //Creamos un bucle para ingresar seis números
         for (int i = 0; i < 6; i++)
         {
-            System.Console.WriteLine($"Ingrese el número {i+1} ganador de la loteria: ");
+            //Pedimos al usuario que ingrese cada numero
+            System.Console.WriteLine($"Ingrese el número {i + 1} ganador de la loteria: ");
             string numGanador = Console.ReadLine();
+            //Añadimos cada numero ingresado a la lista de numeros ganadores
             numsGanadores.Add(numGanador);
         }
 
@@ -18,8 +21,10 @@
         System.Console.WriteLine("============================================================");
         System.Console.WriteLine("");
         
+        //Utilizamos el método sort para ordenalos de menor a mayor
         numsGanadores.Sort();
 
+        //Bucle para imprimir todos los números en orden
         foreach (var numero in numsGanadores)
         {
             System.Console.WriteLine(numero);
