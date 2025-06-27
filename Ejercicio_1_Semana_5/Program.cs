b91a855a3297dccace6a63328ad1a77fd25ae862
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 17:40:37 -0500

    Código comentado

--- Ejercicio_1_Semana_5/Program.cs
+++ Ejercicio_1_Semana_5/Program.cs
@@ -2,8 +2,10 @@
 {
     static void Main(string[] args)
     {
+        //Creamos una lista de materias
         List<string> asignaturas = ["Matemática", "Física", "Química", "Historia", "Lengua"];
 
+        //Con un bucle for imprimimos cada asignatura
         foreach (var asignatura in asignaturas)
         {
             System.Console.WriteLine($"Materia: {asignatura}");
