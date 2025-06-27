a941a7622b471854b87755b47a11a7e075b26f7a
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 16:15:34 -0500

    Fin E4

--- Ejercicio_4_Semana_5/Program.cs
+++ Ejercicio_4_Semana_5/Program.cs
@@ -5,7 +5,7 @@
         List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
         numeros.Reverse();
-
+        
         string listaDeNumeros = string.Join(", ", numeros);
         System.Console.WriteLine(listaDeNumeros);
 
