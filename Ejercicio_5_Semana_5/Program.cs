c37ea7d2e89dd4856bdc4444921aae21dc4fad6c
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-06-27 16:27:03 -0500

    Continuación E5

--- Ejercicio_5_Semana_5/Program.cs
+++ Ejercicio_5_Semana_5/Program.cs
@@ -2,6 +2,31 @@
 {
     static void Main(string[] args)
     {
-        
+        List<string> asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"];
+        List<string> notas = [];
+
+        for (int i = 0; i < asignaturas.Count; i++)
+        {
+            System.Console.WriteLine($"Ingrese la nota de la materia de {asignaturas[i]}: ");
+            string nota = Console.ReadLine();
+            notas.Add(nota);
+        }
+
+        for (int i = 0; i < asignaturas.Count; i++)
+        {
+            if (notas[i] >= 7)
+            {
+                asignaturas.Remove(asignaturas[i]);
+            }
+        }
+
+        System.Console.WriteLine("");
+        System.Console.WriteLine("Materias Reprobadas");
+        System.Console.WriteLine("");
+
+        foreach (var materia in asignaturas)
+        {
+            System.Console.WriteLine(materia);
+        }
     }
 }
