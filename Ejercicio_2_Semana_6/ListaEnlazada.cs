ed159788ecbf1ed39681193c1f5c6ffa4fc9c30a
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-07-06 12:09:03 -0500

    avance

--- Ejercicio_2_Semana_6/ListaEnlazada.cs
+++ Ejercicio_2_Semana_6/ListaEnlazada.cs
@@ -30,7 +30,7 @@ public class ListaEnlazada
         Nodo actual = head;
         while (actual != null)
         {
-            if (valorBuscado == actual.valor)
+            if (valorBuscado == actual.Valor)
             {
                 contador++;
             }
