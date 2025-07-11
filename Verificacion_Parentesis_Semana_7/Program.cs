3dee4188017e3e730314dddcd29257e7a5ef5614
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-07-11 16:57:00 -0500

    Avance

--- Verificacion_Parentesis_Semana_7/Program.cs
+++ Verificacion_Parentesis_Semana_7/Program.cs
@@ -4,5 +4,45 @@ Stack<string> cantSignosAgrupacion = new Stack<string>();
 
 foreach (var e in ecuacion)
 {
-    System.Console.WriteLine(e);
+    if (e == "(" || e == ")" || e == "[" || e == "]" || e == "{" || e == "}")
+    {
+        cantSignosAgrupacion.Push(e);
+    }
 }
+
+int contadorParentsisA = 0;
+int contadorParentsisC = 0;
+int contadorCorchetesA = 0;
+int contadorCorchetesC = 0;
+int contadorLlavesA = 0;
+int contadorLlavesC = 0;
+
+for (int i = 0; i < cantSignosAgrupacion.Count(); i++)
+{
+    if (cantSignosAgrupacion[i] == "(")
+    {
+        contadorParentsisA++;
+    }
+    else if (cantSignosAgrupacion[i] == ")")
+    {
+        contadorParentsisC++;
+    }
+    else if (cantSignosAgrupacion[i] == "[")
+    {
+        contadorCorchetesA++;
+    }
+    else if (cantSignosAgrupacion[i] == "]")
+    {
+        contadorCorchetesC++;
+    }
+    else if (cantSignosAgrupacion[i] == "{")
+    {
+        contadorLlavesA++;
+    }
+    else if (cantSignosAgrupacion[i] == "}")
+    {
+        contadorLlavesC++;
+    }
+
+}
+
