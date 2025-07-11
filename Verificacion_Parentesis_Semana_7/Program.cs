6be1ed0c8fbd4f91ad57acdc90a480300ac8dad3
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-07-11 16:26:35 -0500

    Avance

--- Verificacion_Parentesis_Semana_7/Program.cs
+++ Verificacion_Parentesis_Semana_7/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string ecuacion = "{2 + 3(8/3) + (2/3) / [(2*3)/2]}";
+
+Stack<string> cantSignosAgrupacion = new Stack<string>();
+
+foreach (var e in ecuacion)
+{
+    System.Console.WriteLine(e);
+}
