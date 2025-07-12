563c096cfd5b1254c54d55b679faa5e060d1269b
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-07-12 16:05:32 -0500

    avance

--- Torres_Hanoi_Semana_7/Program.cs
+++ Torres_Hanoi_Semana_7/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numDiscos = 4;
+
+Stack<int> origen = new Stack<int>();
+Stack<int> auxiliar = new Stack<int>();
+Stack<int> destino = new Stack<int>();
+
+for (int i = 0; i < numDiscos; i++)
+{
+    origen.Push(i);
+}
+
+static void solucion()
+{
+    
+}
+
+static void Mover(Stack<int> desde, Stack<int> hacia, string nombreDesde, string nombreHacia)
+{
+    int disco = desde.Pop();
+    hacia.Push(disco);
+    System.Console.WriteLine($"Mover el disco {disco} de {nombreDesde} a {nombreHacia}");
+}
