6790a05d3fda9015165f6ae4d35231ed63763e7a
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-08-19 17:25:52 -0500

    Avance

--- Tarea_Semana_10/Program.cs
+++ Tarea_Semana_10/Program.cs
@@ -9,9 +9,29 @@ for (int i = 0; i < 500; i++)
     {
         CiudadanosPfizer.Add($"Ciudadano {i + 1}");
     }
+    if (i >= 30 && i < 150)
+    {
+        CiudadanosAstraZeneca.Add($"Ciudadano {i + 1}");
+    }
+    if (i > 150)
+    {
+        CiudadanosNoVacunados.Add($"Ciudadano {i + 1}");
+    }
+
+
+}
+
+foreach (var i in CiudadanosNoVacunados)
+{
+    System.Console.Write(i, );
 }
 
 foreach (var i in CiudadanosPfizer)
 {
-    System.Console.WriteLine(i);
+    System.Console.Write(i, );
+}
+
+foreach (var i in CiudadanosAstraZeneca)
+{
+    System.Console.Write(i, );
 }
