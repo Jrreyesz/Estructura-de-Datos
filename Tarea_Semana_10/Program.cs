865bbf24ec073d77759b47b1e7ed4bc8c2e5957e
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-08-19 17:56:13 -0500

    Código Comentado Final

--- Tarea_Semana_10/Program.cs
+++ Tarea_Semana_10/Program.cs
@@ -1,18 +1,22 @@
-﻿
+﻿//Creamos tres conjuntos, uno para no vacunados, otro para pfizer y otro para AstraZeneca
 HashSet<string> CiudadanosNoVacunados = new HashSet<string> { };
 HashSet<string> CiudadanosPfizer = new HashSet<string> { };
 HashSet<string> CiudadanosAstraZeneca = new HashSet<string> { };
 
+//Con un bucle for agregaremos los ciudadanos a sus conjuntos, 500 en total
 for (int i = 0; i <= 500; i++)
 {
+    //75 ciudadanos para pfizer
     if (i < 75)
     {
         CiudadanosPfizer.Add($"Ciudadano {i + 1}");
     }
+    //75 ciudadanos para AstraZeneca, de los cuales 45 tambien tienen pfizer
     if (i >= 30 && i < 105)
     {
         CiudadanosAstraZeneca.Add($"Ciudadano {i + 1}");
     }
+    //ciudadanos no vacunados
     if (i > 105)
     {
         CiudadanosNoVacunados.Add($"Ciudadano {i}");
@@ -21,10 +25,11 @@ for (int i = 0; i <= 500; i++)
 
 }
 
+//Creamos un conjunto con los elementos de la interesección entre los conjuntos de pfizer y astrazeneca
 HashSet<string> AmbasDosis = new HashSet<string>(CiudadanosPfizer);
 AmbasDosis.IntersectWith(CiudadanosAstraZeneca);
 
-
+//Imprimir en pantalla las listas correspondientes
 System.Console.WriteLine("==========================Ciudadanos No Vacunados==========================");
 System.Console.WriteLine("");
 foreach (var c in CiudadanosNoVacunados)
