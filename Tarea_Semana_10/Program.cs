7b09e404d3acf3b18ef5e6d951c2b51ebb1b5703
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-08-19 17:17:10 -0500

    Avance

--- Tarea_Semana_10/Program.cs
+++ Tarea_Semana_10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+HashSet<string> TotalCiudadanos = new HashSet<string> { };
+HashSet<string> CiudadanosPfizer = new HashSet<string> { };
+HashSet<string> CiudadanosAstraZeneca = new HashSet<string> { };
+
+for (int i = 0; i < 500; i++)
+{
+    if (i < 75)
+    {
+        CiudadanosPfizer.Add($"Ciudadano {i + 1}");
+    }
+}
+
+System.Console.WriteLine(CiudadanosPfizer);
