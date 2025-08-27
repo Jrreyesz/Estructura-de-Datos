b7771b806b1ad9cb1f5969b0953d9f3754f5145c
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-08-27 17:01:48 -0500

    Avance

--- Tarea_Semana_11/Program.cs
+++ Tarea_Semana_11/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Dictionary<string, string> palabrasIngles = new Dictionary<string, string>
+{
+    {"time", "tiempo"},
+    {"person", "persona"},
+    {"year", "año"},
+    {"way", "forma"},
+    {"day", "día"},
+    {"thing", "cosa"},
+    {"man", "hombre"},
+    {"world", "mundo"},
+    {"life", "vida"},
+    {"part", "parte"},
+    {"child", "niño"},
+    {"eye", "ojo"},
+    {"place", "lugar"},
+    {"work", "trabajo"},
+    {"week", "semana"}
+};
+
+foreach (var v in palabrasIngles.Values())
+{
+    System.Console.WriteLine(v);
+}
