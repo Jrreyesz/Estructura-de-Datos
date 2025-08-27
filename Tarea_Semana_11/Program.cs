0d40528198de70c29c53ddaec46e6f0a00049aa2
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-08-27 18:21:10 -0500

    Código comentado final

--- Tarea_Semana_11/Program.cs
+++ Tarea_Semana_11/Program.cs
@@ -1,4 +1,5 @@
-﻿Dictionary<string, string> palabrasIngles = new Dictionary<string, string>
+﻿//Creamos el diccionario con las palabras en inglés
+Dictionary<string, string> palabrasIngles = new Dictionary<string, string>
 {
     {"time", "tiempo"},
     {"person", "persona"},
@@ -17,44 +18,58 @@
     {"week", "semana"}
 };
 
+//Creamos un método para traducir la frase que desee el usuario
 void traducirFrase()
 {
+    //Pedimos la frase al usuario
     System.Console.WriteLine("Escriba la frase que desea traducir:");
+    //Almacenamos la frase en una variable
     string frase = Console.ReadLine();
+    //Creamos una nueva variable para la frase traducida que inicie con la frase original
     string frasetraducida = frase;
 
+    //Creamos un bucle para recorrer las claves y valores del diccionario
     foreach (KeyValuePair<string, string> palabra in palabrasIngles)
-        {
-            frasetraducida = frasetraducida.Replace(palabra.Value, palabra.Key);
-        }
-
+    {
+        frasetraducida = frasetraducida.Replace(palabra.Value, palabra.Key);
+    }
+    //Imprime la frase traducida
     System.Console.WriteLine($"La frase traducida es: {frasetraducida}");
 }
 
+//Creamos una función para agregar una palabra
 void agregarPalabra()
 {
+    //Pedimos al usuario la palabra que desea agregar en inglés y la almacenamos en una variable
     System.Console.WriteLine("Escriba la palabra que desea agregar en inglés:");
     string palabraClave = Console.ReadLine();
+    //Pedimos el significado de la palabra al usuario y la almacenamos en una variable
     System.Console.WriteLine("Escriba la palabra en español:");
     string palabraValor = Console.ReadLine();
+    //Con el método Add agregamos el nuevo par clave - valor al diccionario
     palabrasIngles.Add(palabraClave, palabraValor);
 }
 
+//Creamos una función para consultar una palabra, esto nos servirá para asegurarnos de que las palabras nuevas se añadan
 void consultarPalabra()
 {
+    //Pedimos al usuario la palabra que desea buscar en inglés y la almacenamos en una variable
     System.Console.WriteLine("Ingrese la palabra que desea buscar en inglés: ");
     string palaraBuscada = Console.ReadLine();
+    //Con ContainsKey verificamos si clave existe en el diccionario
     if (palabrasIngles.ContainsKey(palaraBuscada.ToLower()))
     {
+        //Imprime el valor de la palabra buscada
         System.Console.WriteLine($"La palabra existe, su traducción es: {palabrasIngles[palaraBuscada]}");
     }
     else
     {
+        //Se imprime si no se encuentra la palabra
         System.Console.WriteLine("Palabra no encontrada");
     }
 }
 
-
+//Creamos el menú
 while (true)
 {
     System.Console.WriteLine("=========================Menú=========================");
