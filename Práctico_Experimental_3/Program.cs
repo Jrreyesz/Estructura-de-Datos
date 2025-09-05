d910d81ef06867f41e4370fd19b0b436da721b6d
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-09-05 16:19:15 -0500

    Avance

--- Práctico_Experimental_3/Program.cs
+++ Práctico_Experimental_3/Program.cs
@@ -10,6 +10,7 @@ void registrarjugadores()
     if (jugadores.ContainsKey(id))
     {
         System.Console.WriteLine("El ID ingresado ya está registrado.");
+        System.Console.WriteLine("");
     }
     else
     {
@@ -17,16 +18,42 @@ void registrarjugadores()
         string nombrejugador = Console.ReadLine();
         jugadores.Add(id, nombrejugador);
         System.Console.WriteLine("El jugador fue registrado correctamente.");
+        System.Console.WriteLine("");
     }
 }
 
+void verjugadoresregistrados()
+{
+    foreach (KeyValuePair<string, string> jugador in jugadores)
+    {
+        System.Console.WriteLine($"Jugador {jugador.Value}, con ID {jugador.Key}");
+    }
+}
+
+void registrarEquipo()
+{
+    System.Console.WriteLine("Ingrese el nombre del equipo:");
+    string nombreequipo = Console.ReadLine();
+    equipos.Add(nombreequipo);
+}
+
+void verequiposregistrados()
+{
+    foreach (string e in equipos)
+    {
+        System.Console.WriteLine(e);
+    }
+}
 
 while (true)
 {
     System.Console.WriteLine("==========================Menú==========================");
     System.Console.WriteLine("");
     System.Console.WriteLine("1. Registrar jugador en la base de datos");
-    System.Console.WriteLine("2. Salir");
+    System.Console.WriteLine("2. Ver jugadores registrados en la base de datos");
+    System.Console.WriteLine("3. Registrar equipo");
+    System.Console.WriteLine("4. Ver equipos registrados");
+    System.Console.WriteLine("5. Salir");
     System.Console.WriteLine("");
 
     System.Console.WriteLine("Elija una opción:");
@@ -37,6 +64,18 @@ while (true)
         registrarjugadores();
     }
     else if (opcion == "2")
+    {
+        verjugadoresregistrados();
+    }
+    else if (opcion == "3")
+    {
+        registrarEquipo();
+    }
+    else if (opcion == "4")
+    {
+        verequiposregistrados();
+    }
+    else if (opcion == "5")
     {
         break;
     }
