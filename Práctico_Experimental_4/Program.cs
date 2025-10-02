7ae9fe84c55f89eade0f844bf4e5caf307a22c65
Jrreyesz <jr.reyesz@uea.edu.ec>
2025-10-02 17:48:46 -0500

    Avance

--- Práctico_Experimental_4/Program.cs
+++ Práctico_Experimental_4/Program.cs
@@ -1,33 +1,39 @@
-﻿using System;
-
-var arbol = new ArbolBinario();
-
-Dictionary<string, int> vuelos = new Dictionary<string, int>
+﻿public class Program
 {
-    {"Iberia", 950},
-    {"Brittish Airways", 150},
-    {"Air France", 120},
-    {"Lufthansa", 135},
-    {"KLM", 115},
-    {"American Airlines", 1100},
-    {"United Airlines", 1050},
-    {"Delta", 1080},
-    {"Air Canada", 990},
-    {"Alitalia", 105},
-    {"Swiss", 160},
-    {"Turkish Airlines", 480},
-    {"SAS", 190},
-    {"Aer Lingus", 185},
-    {"TAP Air Portugal", 95},
-    {"Aeroflot", 550},
-    {"Austrian Airlines", 145},
-    {"LATAM", 1150},
-    {"Aeroméxico", 890},
-    {"Avianca", 980}
-};
+    public static void Main(string[] args)
+    {
+        var arbol = new ArbolBinario();
+        
+        Dictionary<string, int> vuelos = new Dictionary<string, int>
+        {
+            {"Iberia", 950},
+            {"Brittish Airways", 150},
+            {"Air France", 120},
+            {"Lufthansa", 135},
+            {"KLM", 115},
+            {"American Airlines", 1100},
+            {"United Airlines", 1050},
+            {"Delta", 1080},
+            {"Air Canada", 990},
+            {"Alitalia", 105},
+            {"Swiss", 160},
+            {"Turkish Airlines", 480},
+            {"SAS", 190},
+            {"Aer Lingus", 185},
+            {"TAP Air Portugal", 95},
+            {"Aeroflot", 550},
+            {"Austrian Airlines", 145},
+            {"LATAM", 1150},
+            {"Aeroméxico", 890},
+            {"Avianca", 980}
+        };
 
-foreach (int precio in vuelos.Values)
-{
-    Insertar(precio);
+        foreach (int precio in vuelos.Values)
+        {
+            Insertar(precio);
+        }
+
+    }
 }
+
 
